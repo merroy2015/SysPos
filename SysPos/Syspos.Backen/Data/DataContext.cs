fb7997d20bf4016c42b9856d51b62f14d99f0a41
Roy <merroy2015@hotmail.com>
2024-04-20 20:49:37 -0500

    crear base de datos

---
+++ SysPos/Syspos.Backen/Data/DataContext.cs
@@ -0,0 +1,23 @@
+﻿
+using Microsoft.EntityFrameworkCore;
+using Syspos.Shered.Entites;
+
+namespace Syspos.Backen.Data
+{
+    public class DataContext : DbContext
+    {
+         
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Country> Countries { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(c => c.Name).IsUnique();
+        }
+    }
+}
+
